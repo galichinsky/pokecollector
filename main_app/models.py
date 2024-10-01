35a3960aa66a73af076c369c2eadabc6f04a4305
galichinsky <galichinsky@gmail.com>
2024-10-01 16:52:32 -0500

    Style detail page

--- main_app/models.py
+++ main_app/models.py
@@ -4,9 +4,11 @@ from django.db import models
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=100)
+    poke_id = models.IntegerField()
     xp = models.IntegerField()
     type = models.CharField(max_length=100)
-    description = models.TextField()
+    abilities = models.TextField()
+    moves = models.TextField()
     image_url = models.URLField()
 
     def __str__(self):
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   # route for cats index
   path('pokemon/', views.index, name='pokemon-index'),
+  path('pokemon/<int:poke_id>/', views.poke_detail, name='poke-detail'),
 ]
